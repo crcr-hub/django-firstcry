47314f04f47db483bffe2ae8a2290a01973adc43
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2024-05-10 11:32:01 +0530

    db connection

--- firstcry/settings.py
+++ firstcry/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'firstcry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project_firstcry',
-        'USER' : 'postgres',
-        'PASSWORD' : 'raijo',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'firstcry',
-#         'USER': 'superuser',
-#         'PASSWORD': 'superuser',
-#         'HOST': 'firstcrydb.cte48u6cuzjl.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'NAME': 'project_firstcry',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 'raijo',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'firstcry',
+        'USER': 'superuser',
+        'PASSWORD': 'superuser',
+        'HOST': 'firstcrydb.cte48u6cuzjl.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
