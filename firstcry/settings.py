b2a1227d6777e75cacc4df08344329d553481066
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2024-05-10 19:32:25 +0530

    changes in settings.py

--- firstcry/settings.py
+++ firstcry/settings.py
@@ -75,19 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'firstcry.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'project_firstcry',
-#         'USER' : 'postgres',
-#         'PASSWORD' : 'raijo',
-#         'HOST' : 'localhost',
-#         'PORT' : '5432'
-#     }
-# }
 
 DATABASES = {
     'default': {
